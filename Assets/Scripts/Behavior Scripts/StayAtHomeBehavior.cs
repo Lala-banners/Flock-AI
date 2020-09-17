4f1e5c233b3520a9476d5c7a6ffd4c176c6f6d9e
Lala-banners <lala.l.banno@hotmail.com>
2020-09-17 12:55:39 +1000

    Created new behavior

---
+++ Assets/Scripts/Behavior Scripts/StayAtHomeBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behavior/Stay at Home")]
+
+public class StayAtHomeBehavior : FlockBehavior
+{
+    [SerializeField]
+    private Vector2 center;
+    [SerializeField]
+    public float radius = 15;
+
+    public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        //Direction to the center
+        //Magnitude will be the distance to the center
+        Vector2 centerOffset = center - (Vector2)agent.transform.position;
+        float t = centerOffset.magnitude / radius;
+
+        //If t less than the radius, do not change the agents
+        if (t < 0.9f)
+        {
+            return Vector2.zero;
+        }
+        //If not, change the offset
+        return centerOffset * t * t;
+    }
+}
---
+++ Assets/Scripts/Behavior Scripts/SteeredCohesionBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behavior/Steered Cohesion")]
+
+public class SteeredCohesionBehavior : FlockBehavior
+{
+    Vector2 currentVelocity;
+    public float agentSmoothTime = 0.5f;
+
+    public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        //If no neighbors, return no adjustment
+        if (context.Count == 0)
+        {
+            return Vector2.zero;
+        }
+
+        //add all points together and average
+        Vector2 cohesionMove = Vector2.zero;
+        
+        foreach(Transform item in context)
+        {
+            cohesionMove += (Vector2)item.position;
+        }
+        cohesionMove /= context.Count;
+
+        cohesionMove -= (Vector2)agent.transform.position;
+        //gradually changes a vector toward a desired goal over time - 
+        //Asks for current position (agent.transform.up), target position(cohesionMove), 
+        //reference to speed(currentVelocity), smoothTime(time taken to reach target),
+        //ref works similar to normal parameter but can be modified 
+        //default time.deltaTime
+        cohesionMove = Vector2.SmoothDamp(agent.transform.up, cohesionMove, ref currentVelocity, agentSmoothTime);
+        return cohesionMove;
+    }
+}
