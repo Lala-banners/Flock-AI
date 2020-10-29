8977608ea39e8537e16094731d87e4e38eac2b78
Lala-banners <lala.l.banno@hotmail.com>
2020-10-29 13:49:49 +1100

    Started Inheritance & Polymorphism assessment

---
+++ Assets/Scripts/InheritancePoly Scripts/Life.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Life : MonoBehaviour
+{
+    //[Header("Life")]
+
+    //[Space]
+
+    //[Header("Predator")]
+
+    //[Space]
+
+    //[Header("Prey")]
+
+    #region Variables
+    public GameObject[] PredatorWaypoints;
+    public GameObject[] PreyWaypoints;
+    public GameObject[] Obstacles;
+    
+    #endregion
+
+    #region Functions
+
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+    #endregion
+
+
+}
